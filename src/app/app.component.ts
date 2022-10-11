3af950ad988f9f692efdd6387816266717b60fdc
DaedalusAlpha <mattrmcc@gmail.com>
2022-10-11 09:41:23 -0500

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DonutDetail } from './models/donut';
+import { DonutService } from './services/donut.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'donut-shop';
+
+  constructor(
+    private route: ActivatedRoute,
+    private donutService: DonutService
+  ) {}
+
+  cart: DonutDetail[] = [];
+
+  ngOnInit(): void {
+    this.cart = this.donutService.getCart();
+  }
+
+  getCartSize(): number {
+    return this.cart.length;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
